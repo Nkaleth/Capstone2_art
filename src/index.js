96f3ee8c36f20fd398c3f8a38dbe04adbf602191
Nkaleth <nseguralu@gmail.com>
2023-02-07 11:33:49 -0500

    Add Changes to basic home page on index.js to be clean

--- src/index.js
+++ src/index.js
@@ -1,13 +1,2 @@
-import _ from 'lodash';
+/* import _ from 'lodash'; */
 import './style.css';
-
-function component() {
-  const element = document.createElement('div');
-
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Art', 'Gallery!'], ' ');
-  element.classList.add('art');
-  return element;
-}
-
-document.body.appendChild(component());
