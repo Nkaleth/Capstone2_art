b648601a0ec1d1aee101b92b671675bd922bd64b
Nkaleth <nseguralu@gmail.com>
2023-02-08 14:48:24 -0500

    Add starting working with comments

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import './style.css';
 import { openComments } from './modules/comments_popup.js';
 
 const buttonTest = document.getElementById('436453');
-// const popUpComments = document.querySelector('.popUpComments');
+const popUpComments = document.querySelector('.popUpComments');
 
 buttonTest.addEventListener('click', () => {
   /* here goes a code to detect the ID button of the painting */
-  openComments(6266929); /* here use the function with the ID */
+  openComments(626692); /* here use the function with the ID */
 });
