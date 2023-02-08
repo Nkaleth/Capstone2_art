8f435bbc10fd88673b7b40c68db34b7d22812cb4
Nkaleth <nseguralu@gmail.com>
2023-02-08 13:00:42 -0500

    Add some comments for the better understading when merge

--- src/index.js
+++ src/index.js
@@ -5,5 +5,6 @@ const buttonTest = document.getElementById('436453');
 // const popUpComments = document.querySelector('.popUpComments');
 
 buttonTest.addEventListener('click', () => {
-  openComments(626692);
+  /* here goes a code to detect the ID button of the painting */
+  openComments(6266929); /* here use the function with the ID */
 });
