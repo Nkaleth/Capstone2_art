281524662bfeace3fbe16241a5c27d6f4ec5bfa3
Nkaleth <nseguralu@gmail.com>
2023-02-08 12:11:33 -0500

    Add button open popUp functionality

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import './style.css';
-import { Xclose, openComments } from './modules/comments_popup.js';
+import { openComments } from './modules/comments_popup.js';
 
-const xclose = document.getElementById('xclose');
 const buttonTest = document.getElementById('436453');
 // const popUpComments = document.querySelector('.popUpComments');
 
-xclose.src = Xclose;
-
 buttonTest.addEventListener('click', () => {
-  openComments(488126);
+  openComments(339751);
 });
--- src/modules/comments_popup.js
+++ src/modules/comments_popup.js
@@ -6,9 +6,38 @@ const openComments = (id) => {
     const request = new Request(link);
     const response = await fetch(request);
     const data = await response.json();
-    console.log(data);
-    // console.log(data.accessionYear, data.artistNationality);
-    // console.log(data.medium, data.artistDisplayName, data.primaryImage);
+    const stringCommentPopup = `<article class="popUpComments">
+                              <img id="xclose" class="xclose" src = "${Xclose}">
+                              <section class="imageComments">
+                                  <img src=${data.primaryImage} alt="" srcset="">
+                              </section>
+                              <section class ="detailsComments" >
+                                  <p class="titleComments">${data.title}</p>
+                                  <ul class="dataPaintingComments">
+                                      <li>Year: ${data.accessionYear}</li>
+                                      <li>Artist : ${data.artistDisplayName}</li>
+                                      <li>Nationality : ${data.artistNationality}</li>
+                                      <li>Medium : ${data.medium}</li>
+                                  </ul>
+                              </section>
+                              <section class="divComments">
+                                  <p class="countComments">Comments 2</p>
+                                  <ul class="listComments">
+                                      <li class="userComment"> 07/02/2023 Alex: I'd love to buy it!</li>
+                                  </ul>
+                              </section>
+                              <section class="addComment">
+                                  <p>Add a comment</p>
+                                  <form class="formComment" action="">
+                                      <input class="username" type="text" name="" id="username" placeholder="Your name" required>
+                                      <input class="comment" type="text" name="" id="comment" placeholder="Your insights" required>
+                                      <button class="addCommentButton" type="submit">Comment</button>
+                                  </form>
+                              </section>
+                          </article>`;
+    const popUpCommentsContainer = document.querySelector('.containerCommentsPopUp');
+
+    popUpCommentsContainer.innerHTML = stringCommentPopup;
   };
   getdata();
 };
