da7ded55dda9fec712fd6d5187c0ba83722c14af
Nkaleth <nseguralu@gmail.com>
2023-02-08 09:59:49 -0500

    Add ButtonTest for popup comments

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-/* import _ from 'lodash'; */
 import './style.css';
 import { Xclose, openComments } from './modules/comments_popup.js';
 
@@ -12,18 +10,3 @@ xclose.src = Xclose;
 buttonTest.addEventListener('click', () => {
   openComments(488126);
 });
-=======
-import _ from 'lodash';
-import './style.css';
-
-function component() {
-  const element = document.createElement('div');
-
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Art', 'Gallery!'], ' ');
-  element.classList.add('art');
-  return element;
-}
-
-document.body.appendChild(component());
->>>>>>> development
